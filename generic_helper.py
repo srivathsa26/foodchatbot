dca6a9e97b45b14dceb9022778c11f776c1913bb
srivathsa26 <156283275+srivathsa26@users.noreply.github.com>
2024-02-04 10:21:15 +0530

    Add files via upload

---
+++ generic_helper.py
@@ -0,0 +1,15 @@
+import re
+
+def extract_session_id(session_str: str):
+
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string=match.group(1)
+        return extracted_string
+    return ""
+
+def get_str_from_food_dict(food_dict: dict):
+    return ", ".join([f"{int(value)}{key}" for key, value in food_dict.items()])
+if __name__=="__main__":
+    print(get_str_from_food_dict({"samosa": 2,"chhole": 5}))
+#print(extract_session_id("projects/mira-chatbot-for-food-del-agvd/agent/sessions/cee461ff-b8a4-5f22-4bc5-90d345d7bd81/contexts/ongoing-order"))
